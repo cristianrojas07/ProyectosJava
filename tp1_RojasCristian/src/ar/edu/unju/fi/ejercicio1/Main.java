82d71ec35ed3e10ebaf4e0648ae5d1022a593b24
Cristian Rojas <cristianrdev@gmail.com>
2024-04-08 22:57:38 -0300

    Ejercicio1 - Uso de Variables

---
+++ tp1_RojasCristian/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Ejercicio 1
+		int variable1 = 8;
+		int variable2 = 5;
+		int variable3 = 1;
+		int variable4 = 20;
+		int variable5 = 9;
+		
+		float promedio = (variable1 + variable2 + variable3 + variable4 + variable5) / 5f;
+		
+		System.out.println("El promedio es: " + promedio);
+	}
+
+}
