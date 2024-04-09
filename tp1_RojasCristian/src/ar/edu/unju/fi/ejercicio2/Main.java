911557a4d51c62966c4b112c005a7113067e88d3
Cristian Rojas <cristianrdev@gmail.com>
2024-04-08 23:32:49 -0300

    Ejercicio 2 - Uso de variables

---
+++ tp1_RojasCristian/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Ejercicio 2
+		String Pais = "Argentina";
+		int Edad = 27;
+		float AlturaEdificio = 4.7f;
+		float Precio = 15.74f;
+		String Telefono = "388-5184701";
+		double CalculoCoseno = Math.cos(0.5);
+
+		System.out.println("Nombre de Pais: " + Pais);
+		System.out.println("Edad: " + Edad);
+		System.out.println("Altura del Edificio: " + AlturaEdificio);
+		System.out.println("Precio del Producto: " + Precio);
+		System.out.println("Número de Telefono: " + Telefono);
+		System.out.println("Cálculo de Coseno: " + CalculoCoseno);
+	}
+
+}
